55e56d5693b04b8e4af93887691a10ce85d52d51
Chris Gresock <chris.gresock@gmail.com>
2019-11-26 14:51:14 -0500

    Created more reliable matching algorithm, although still pretty rough

--- src/components/SecretSantaList.jsx
+++ src/components/SecretSantaList.jsx
@@ -38,25 +38,71 @@ const people = {
   }
 }
 
+// secretSanta = sender to resolve. Available is an array of all people with people to send to to potentially swap
+const resolveSecretSanta = (secretSanta, available = [], unchosenAvailable = [], skipStandard = false) => {
+  if(secretSanta.giftTo === 0 || secretSanta.giftTo){
+    //Conflict resolved
+    return true
+  }
+
+  if(!skipStandard){// Standard random finder
+    let availId = []
+    let index = 0
+    unchosenAvailable.forEach((u)=>{
+      if(!secretSanta.blacklist.includes(u.id) && u.id !== secretSanta.id){availId.push(index)}
+      index++
+    })
+    if(availId.length>0){
+      const randomK = Math.floor((availId.length-1)*Math.random())
+      const randomAvailIndex = availId[randomK]
+      const randomAvail = unchosenAvailable[randomAvailIndex]
+      secretSanta.giftTo = randomAvail.id
+      //resolve index for chosen random
+      unchosenAvailable.splice(randomAvailIndex, 1)
+      return resolveSecretSanta(secretSanta, available, unchosenAvailable, skipStandard=true)
+    }else{
+      return resolveSecretSanta(secretSanta, available, unchosenAvailable, skipStandard=true)
+    }
+  }
+
+  const randomJ = Math.floor((unchosenAvailable.length -1) * Math.random())
+  const unchosen = unchosenAvailable[randomJ]
+  const randomI = Math.floor((available.length - 1) * Math.random())
+  const chosen = available[randomI]
+  const chosenGiftTo = people[chosen.giftTo]
+
+  if(secretSanta.blacklist.includes(chosen.giftTo)){
+    available.splice(randomI, 1)
+    return resolveSecretSanta(secretSanta, available, unchosenAvailable)
+  }
+  if(chosen.blacklist.includes(unchosen.id)){
+    available.splice(randomI, 1)
+    return resolveSecretSanta(secretSanta, available, unchosenAvailable)
+  }
+  if(chosenGiftTo.blacklist.includes(secretSanta.id)){
+    available.splice(randomI, 1)
+    return resolveSecretSanta(secretSanta, available, unchosenAvailable)
+  }
+
+  // chosenGiftTo can now be swapped with unchosen
+  chosen.giftTo = unchosen.id
+  secretSanta.giftTo = chosenGiftTo.id
+  unchosenAvailable.splice(randomJ,1)
+
+  return resolveSecretSanta(secretSanta, available, unchosenAvailable)
+
+}
+
 const chooseSecretSanta = () => {
-  let unchosen = Object.keys(people)
-  const chosen = {...people}
+  let unchosen = Object.values(people)
+  const chosen = {}
 
   Object.values(people).forEach((person)=>{
     // Array for possible index options
-    const indexArray = []
-    for(let i = 0; i < unchosen.length+1;i++){
-      const filteredI = parseInt(unchosen[i])
-      if(person.id !== filteredI && !person.blacklist.includes(filteredI) && unchosen.includes(filteredI.toString())){
-        indexArray.push(filteredI)
-      }
+    const resolved = resolveSecretSanta(person, Object.values(chosen), unchosen)
+    if(resolved){
+      chosen[person.id] = person
     }
-    const chosenIndex = indexArray[Math.floor(Math.random() * (indexArray.length-1))]
-    console.log(chosenIndex)
-    const chosenPerson = people[chosenIndex]
-    chosen[person.id].giftTo = chosenPerson.id
-    chosen[chosenPerson.id].giftFrom = person.id
-    unchosen = unchosen.filter((pId)=>parseInt(pId) !== chosenPerson.id)
   })
 
   return chosen
@@ -65,14 +111,6 @@ const chooseSecretSanta = () => {
 const SecretSanta = ({secretSantaId, blacklist = true}) => {
   const person = people[secretSantaId]
   const giftTo = people[person.giftTo]
-  const renderBlacklist = () => {
-    if(!blacklist){return null}
-    return (
-      <div>
-        - Blacklist: {person.blacklist.map((id)=><SecretSanta key={`bl${person.id}.${id}`} secretSantaId={id} blacklist={false} />)}
-      </div>
-      )
-  }
   const renderGiftTo = () => {
     if(!blacklist){return null}
     return (
@@ -81,8 +119,10 @@ const SecretSanta = ({secretSantaId, blacklist = true}) => {
   }
   return (
     <div>
-      {person.name} {renderBlacklist()}
+      {person.name}
+      <br/>
       {renderGiftTo()}
+      <br/>
       <br/>
     </div>
   )
