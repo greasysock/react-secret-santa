16b5ac079d8aec90c77a94017659e34562909dce
chris <chris.gresock@gmail.com>
2019-11-29 17:23:45 -0500

    switch to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import SecretSanta from './pages/SecretSanta'
 import ShowRecipient from './pages/ShowRecipient'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Route exact path="/" component={SecretSanta}/>
       <Route exact path="/my_recipient" component={ShowRecipient}/>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
